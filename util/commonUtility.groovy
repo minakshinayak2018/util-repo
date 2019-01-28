919290cd611856cbb2efb27256473625e085235b
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-28 22:48:17 +0530

    common template

--- util/commonUtility.groovy
+++ util/commonUtility.groovy
@@ -1,55 +1,74 @@
-pipeline {
-	agent any
-        	stages {
-		stage('LOAD PROPERTIES FILES') {
-                  steps {
-                       script {
-		       	 gitProp = readProperties file:'./propertiesFiles/git.properties'
-			       commonProp = readProperties file:'./propertiesFiles/common.properties'
-			       artifactoryProp = readProperties file:'./propertiesFiles/artifactory.properties'
-			       deployProps = readProperties file:'./propertiesFiles/deploy.properties'
-			       configProp = load commonProp.configFile
-			       echo 'LOAD SUCCESS'
-				         }
-			       }
-			   }
-			 stage('READ GIT') {
-                  steps {
-		  	              script {
-			                     git url: gitProp.gitUrl,
-                           branch: gitProp.branchName
-                           echo 'READ SUCCESS'
-				                    }
-					               }
-				             }
-			      stage('SONAR SCAN') {
-                  steps {
-		  	            script {
-			                configProp.scan()
-				             }
-				         }
-			        }
-			      stage('BUILD') {
-                  steps {
-		  	               script {
-			                    configProp.build()
-			                      }
-				               }
-			            }
-			      stage('UPLOAD ARTIFACT') {
-                             steps {
-		  	                         script {
-			                       configProp.artifactory()
-			                                }
-				                           }
-			              }
-			     stage('DEPLOY') {
-                         steps {
-		  	                     script {
-			                        configProp.deploy()
-			                       deleteDir()
-			                 }
-                }         
-            }    
-     	}
+   
+            
+def build(){
+			try{
+				dir(gitProps.path){
+					sh commonProps.mavenClean
+					status = "SUCCESSFUL"
+					echo 'BUILD SUCCESS'
+						}
+					}
+			catch (e) {
+					status = "FAILED"
+						} 
+			finally {
+					notifyBuild(status)
+					} 
+			}
+def scan(){                  
+		dir(gitProps.path){
+			sh commonProps.buildSonarScan
+			echo 'SONAR SCAN SUCCESS'
+			}
+          } 
+def artifactory(){
+				server = Artifactory.server artifactoryProps.artifactServer
+				uploadSpec = """{
+                		"files": [
+                    		{	
+ 					"build": "${env.BUILD_NUMBER}",
+                       			"pattern": "target/*.war",
+                        		"target": "demo-java/target/${env.BUILD_NUMBER}/"
+                    		}
+                    		]
+                		}"""
+            			server.upload(uploadSpec)
+				echo 'ARTIFACT SUCCESS'
+				}
+				
+def deploy(){
+			try	{
+				sh deployProps.dockerContainerId
+				output=readFile('result').trim()
+				if(output!=null)
+					{
+						sh deployProps.dockerContainerRm
+					}
+				}catch (err)
+					{
+						echo 'DELETE FAILED'
+					}
+			sh deployProps.dockerDeploy
+			sh deployProps.dockerRestart
+			echo 'DEPLOY SUCCESS'				
+		}
+					
+
+def notifyBuild(String buildStatus)
+{
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
+    <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
+	
+ emailext (
+      subject: subject,
+      body: details,
+      to: commonProps.recipients
+    )
+ 
 }
+
+
+return this
