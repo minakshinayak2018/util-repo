e33f39a4db4d113664d60cf371e9d9b5f7c85230
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-28 13:17:46 +0530

    Added CoomonUtility.groovy file

---
+++ commonUtility.groovy
@@ -0,0 +1,54 @@
+pipeline {
+	agent any
+        	stages {
+		stage('LOAD PROPERTIES FILES') {
+                  steps {
+                       script {
+		       	 gitProp = readProperties file:'propertiesFiles/git.properties'
+			       commonProp = readProperties file:'propertiesFiles/common.properties'
+			       artifactoryProp = readProperties file:'propertiesFiles/artifactory.properties'
+			       deployProps = readProperties file:'propertiesFiles/deploy.properties'         
+			       echo 'LOAD SUCCESS'
+				         }
+			       }
+			   }
+			 stage('READ GIT') {
+                  steps {
+		  	              script {
+			                     git url: gitProp.gitUrl,
+                           branch: gitProp.branchName
+                           echo 'READ SUCCESS'
+				                    }
+					               }
+				             }
+			      stage('SONAR SCAN') {
+                  steps {
+		  	            script {
+			                configProp.scan()
+				             }
+				         }
+			        }
+			      stage('BUILD') {
+                  steps {
+		  	               script {
+			                    configProp.build()
+			                      }
+				               }
+			            }
+			      stage('UPLOAD ARTIFACT') {
+                             steps {
+		  	                         script {
+			                       configProp.artifactory()
+			                                }
+				                           }
+			              }
+			     stage('DEPLOY') {
+                         steps {
+		  	                     script {
+			                        configProp.deploy()
+			                       deleteDir()
+			                 }
+                }         
+            }    
+     	}
+}
