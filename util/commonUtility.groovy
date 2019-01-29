cd2cee202a706b99bf6e1ba0687861a6c1357fc2
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-29 22:58:49 +0530

    updated notifyBuild

--- util/commonUtility.groovy
+++ util/commonUtility.groovy
@@ -1,6 +1,6 @@
-def notifyBuild(String buildStatus)
+def notifyBuild()
 {
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def subject = "${env_BUILD_STATUS}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
   def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
