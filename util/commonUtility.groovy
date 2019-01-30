d9f566e0fbc0c23012f3766ee42c5deb268eaccb
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-30 12:04:32 +0530

    debug

--- util/commonUtility.groovy
+++ util/commonUtility.groovy
@@ -5,7 +5,6 @@ def notifyBuild()
   def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
     <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
-def recipients = emailextrecipients([ [$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider']])	
 	
  emailext (
       attachLog: true,
