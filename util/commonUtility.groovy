bf8c300bdd7a7f40936b4a8442f0ee4d35aa0242
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-30 12:00:49 +0530

    fix

--- util/commonUtility.groovy
+++ util/commonUtility.groovy
@@ -5,15 +5,17 @@ def notifyBuild()
   def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
     <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
+def recipients = emailextrecipients([ [$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider']])	
 	
  emailext (
       attachLog: true,
       compressLog: true,	 
       subject: subject,
       body: details,
-      to: commonProps.recipients
+      mail to: commonProps.recipients
     )
   }
+
    def checkOutRepo(String repo, branch, String credentials){
     git repo,branch,credentials
 }
